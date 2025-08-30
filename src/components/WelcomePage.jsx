65a93b31b16d225f613a25715eed82b7c2443cc8
Abash Ansari <ansariabash2004@gmail.com>
2025-08-30 02:41:16 -0400

    created a component for welcome page

---
+++ src/components/WelcomePage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import CodeIcon from '@mui/icons-material/Code';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import CoffeeIcon from '@mui/icons-material/Coffee';
+
+
+export default function WelcomePage() {
+    return (
+        <div className="container vh-100 d-flex justify-content-center align-items-center text-center" id='home'>
+            <div className="text">
+                <div className='container d-flex'>
+                    <CodeIcon />
+                    <FavoriteIcon />
+                    <CoffeeIcon />
+                </div>
+                <h1 className="display-4">Welcome To My <br />
+                    Portfolio Website</h1>
+                <button className='mt-4'>Download Resume</button>
+            </div>
+        </div>
+    )
+}
