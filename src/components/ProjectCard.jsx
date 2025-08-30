9107031d98369c20335578dc1af63cccc8665863
Abash Ansari <ansariabash2004@gmail.com>
2025-08-30 11:02:32 -0400

    created a card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function ProjectCard() {
+export default function ProjectCard({title,desc}) {
   return (
-    <div>ProjectCard</div>
+    <div>
+      <div className="card text-center mb-3 ml-6 mt-6" style={{width: "18rem"}}>
+  <div className="card-body">
+    <h5 className="card-title">{title}</h5>
+    <p className="card-text">{desc}</p>
+  </div>
+</div>
+    </div>
   )
 }
