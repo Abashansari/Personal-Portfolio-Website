7213e3df8d4218a847409e8dfe592568aa2c61e8
Abash Ansari <ansariabash2004@gmail.com>
2025-08-30 11:02:44 -0400

    imported a card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='container'>
+      <h1>My Projects</h1>
+      <div className='row'>
+        <div className='col-md-4 d-flex justify-content-between align-itens-center'>
+      <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
+      <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
+      <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
+        </div>
+      </div>
+
+      <div className='row'>
+        <div className='col-md-4 d-flex justify-content-between align-itens-center'>
+      <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
+      <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
+      <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
+        </div>
+      </div>
+      
+    </div>
   )
 }
