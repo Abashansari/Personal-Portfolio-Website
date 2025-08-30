94f2f0ac9bd240a496d73dbe05baea5369d6399e
Abash Ansari <ansariabash2004@gmail.com>
2025-08-30 05:14:28 -0400

    created a component to show my skills

---
+++ src/components/TechnologySkill.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function TechnologySkill() {
+  return (
+    <div className='container'>
+        <h1>Technology I worked with</h1>
+        <div className='d-flex justify-content-between align-items-center'>
+            <img src='' alt='html'/>
+            <img src='' alt='css'/>
+            <img src='' alt='javaScript'/>
+            <img src='' alt='node Js'/>
+            <img src='' alt='express Js'/>
+            <img src='' alt='react'/>
+            <img src='' alt='bootstrap'/>
+            <img src='' alt='bulma'/>
+            <img src='' alt='materialUI'/>
+        </div>
+    </div>
+  )
+}
