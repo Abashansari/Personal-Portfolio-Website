be2a93ab5c7f8d1eaa881c9580579695076d1567
Abash Ansari <ansariabash2004@gmail.com>
2025-08-30 11:02:16 -0400

    added images

--- src/components/TechnologySkill.jsx
+++ src/components/TechnologySkill.jsx
@@ -1,20 +1,47 @@
 import React from 'react'
+import Html from '/Html.png'
+import Css from '/Css.png'
+import JS from '/JS.png'
+import NodeJs from '/NodeJs.png'
+import Reactjs from '/Reactjs.jpeg'
+import ExpressJs from '/ExpressJs.png'
+import Bootstrap from '/Bootstrap.png'
+import MaterialUI from '/MaterialUI.png'
 
 export default function TechnologySkill() {
   return (
-    <div className='container'>
-        <h1>Technology I worked with</h1>
-        <div className='d-flex justify-content-between align-items-center'>
-            <img src='' alt='html'/>
-            <img src='' alt='css'/>
-            <img src='' alt='javaScript'/>
-            <img src='' alt='node Js'/>
-            <img src='' alt='express Js'/>
-            <img src='' alt='react'/>
-            <img src='' alt='bootstrap'/>
-            <img src='' alt='bulma'/>
-            <img src='' alt='materialUI'/>
+    <div className="container text-center">
+      <h1 className="mb-4">Technology I worked with</h1>
+
+      <div className="row justify-content-center mt-6">
+        <div className="col-6 col-md-3 mb-4">
+          <img src={Html} className="img-fluid" style={{ width: '120px' }} alt="html" />
         </div>
+        <div className="col-6 col-md-3 mb-4">
+          <img src={Css} className="img-fluid" style={{ width: '120px' }} alt="css" />
+        </div>
+        <div className="col-6 col-md-3 mb-4">
+          <img src={JS} className="img-fluid" style={{ width: '200px' }} alt="javaScript" />
+        </div>
+        <div className="col-6 col-md-3 mb-4">
+          <img src={Reactjs} className="img-fluid" style={{ width: '120px' }} alt="react" />
+        </div>
+      </div>
+
+      <div className="row justify-content-center">
+        <div className="col-6 col-md-3 mb-4">
+          <img src={NodeJs} className="img-fluid" style={{ width: '120px' }} alt="node Js" />
+        </div>
+        <div className="col-6 col-md-3 mb-4">
+          <img src={ExpressJs} className="img-fluid" style={{ width: '150px' }} alt="express Js" />
+        </div>
+        <div className="col-6 col-md-3 mb-4">
+          <img src={Bootstrap} className="img-fluid" style={{ width: '100px' }} alt="bootstrap" />
+        </div>
+        <div className="col-6 col-md-3 mb-4">
+          <img src={MaterialUI} className="img-fluid" style={{ width: '120px' }} alt="materialUI" />
+        </div>
+      </div>
     </div>
   )
 }
