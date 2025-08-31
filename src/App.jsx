f72a89b1d40ea3f04a068344150a5da51fc08f8e
Abash Ansari <ansariabash2004@gmail.com>
2025-08-31 03:36:34 -0400

    removed all components and only imported Portfolio component

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar'
-import WelcomePage from './components/WelcomePage'
-import Home from './components/Home'
-import About from './components/About'
+import Portfolio from './components/Portfolio'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar />
-      <WelcomePage />
-      <Home />
-      <About />
+      <Portfolio />
     </>
   )
 }
