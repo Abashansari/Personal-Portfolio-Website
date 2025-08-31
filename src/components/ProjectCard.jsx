8a3e0db64223e88b62813d579fdef54c64127d8f
Abash Ansari <ansariabash2004@gmail.com>
2025-08-31 03:08:47 -0400

    increased the width of card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ProjectCard({title,desc}) {
   return (
     <div>
-      <div className="card text-center mb-3 ml-6 mt-6" style={{width: "18rem"}}>
+      <div className="card text-center mb-3 ml-6 mt-6" style={{width: "30rem"}}>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{desc}</p>
