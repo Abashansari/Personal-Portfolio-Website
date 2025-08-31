fd05bc4f4baf2a4f5a9db703820bcbdced51200e
Abash Ansari <ansariabash2004@gmail.com>
2025-08-31 04:04:31 -0400

    added a icon and removed form tag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import LaptopChromebookIcon from '@mui/icons-material/LaptopChromebook';
 
 export default function Navbar() {
     return (
@@ -9,19 +10,17 @@ export default function Navbar() {
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <a className="navbar-brand" href="#">Portfolio</a>
+                <a className="navbar-brand" href="#"> <LaptopChromebookIcon fontSize='large' className='me-2' /> Abash Ansari</a>
 
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-                    <form className="d-flex ml-6" role="search">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
 
-                            <li className="nav-item"><a className="nav-link active" aria-current="page" href="#home">Home</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#project" >Project</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#contact" >Contact</a></li>
+                        <li className="nav-item"><a className="nav-link active" aria-current="page" href="#home">Home</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#project" >Project</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#contact" >Contact</a></li>
 
-                        </ul>
-                    </form>
+                    </ul>
                 </div>
             </div>
         </nav>
