e99c7bd9cb3949b41cd252960deda5f2e9debfd1
Abash Ansari <ansariabash2004@gmail.com>
2025-08-31 03:09:57 -0400

    removed 2 projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,20 +7,17 @@ export default function Projects() {
       <h1>My Projects</h1>
       <div className='row'>
         <div className='col-md-4 d-flex justify-content-between align-itens-center'>
-      <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
-      <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
-      <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
+          <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
+          <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
         </div>
       </div>
 
       <div className='row'>
         <div className='col-md-4 d-flex justify-content-between align-itens-center'>
-      <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
-      <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
-      <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
+          <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
+          <ProjectCard title='abash' desc='he is great man sjnks sjfskjf skjfslo sfjeosie jsdvosidr seiosejf' />
         </div>
       </div>
-      
     </div>
   )
 }
