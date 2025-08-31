210fa825d21304bca537631a536e4960efc8e69a
Abash Ansari <ansariabash2004@gmail.com>
2025-08-31 03:14:51 -0400

    Created a component for contact's intro

---
+++ src/components/ContactDesc.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ContactDesc() {
+    return (
+        <div className='container'>
+            <h2 className="fw-bold mb-3">Contact Me</h2>
+            <p className="text-start">
+                Feel free to reach out using the form below or through my contact details.
+            </p>
+        </div>
+    )
+}
